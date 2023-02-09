06716a584049fb677ee8c7f70c0ca26aec073083
Laura Perez <laurap48@hotmail.com>
2023-02-09 18:25:36 +0100

    Pintar el listado de noticias en el componente
    Pintar los datos del array noticias en el componente con un reduce y añadir estilos a styles.css para que se apliquen al html

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -14,6 +14,7 @@ export class BlogComponent {
   imagenURL: string = "";
   texto: string = "";
   contId: number = 3; //contador de ids
+  listadoHTML: string= "";
   arrNoticias: Noticia[] = [];
 
   constructor () {
@@ -23,15 +24,32 @@ export class BlogComponent {
         title: 'Neque porro quisquam est qui dolorem ipsum',
         date: '04/02/2023',
         imageURL: 'https://via.placeholder.com/800',
-        text: 'Phasellus hendrerit est id finibus condimentum. Curabitur malesuada justo at ligula varius ultricies. Duis a nisl ligula. Praesent arcu lorem, sagittis at metus non, posuere fermentum diam. Quisque mollis arcu eu sapien fringilla malesuada. Nulla rhoncus erat at urna semper, id dignissim felis ultricies. Sed nec urna eu arcu euismod aliquam eu sed nisi. Donec ultrices sollicitudin arcu, at aliquam tellus ornare eu. Etiam viverra aliquet sollicitudin. Integer tempus urna ipsum. Aenean turpis nisi, scelerisque vitae nulla non, malesuada tincidunt massa. Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas gravida, nisl id accumsan dapibus, metus neque viverra quam, in aliquet ex mauris rhoncus lacus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.'
+        text: '<p> hendrerit est id finibus condimentum. Curabitur malesuada justo at ligula varius ultricies. Duis a nisl ligula. Praesent arcu lorem, sagittis at metus non, posuere fermentum diam. Quisque mollis arcu eu sapien fringilla malesuada. Nulla rhoncus erat at urna semper, id dignissim felis ultricies. Sed nec urna eu arcu fermentum diam euismod aliquam eu sed nisi.</p><p>Donec ultrices sollicitudin arcu, at aliquam tellus ornare eu alesuada elementum. Etiam viverra aliquet sollicitudin. Integer tempus urna ipsum. Aenean turpis nisi, scelerisque vitae nulla non, malesuada tincidunt massa.</p><p>Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas gravida, nisl id accumsan dapibus, metus neque viverra quam, in aliquet ex mauris rhoncus lacus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.</p>'
       },
       {
         id: 2,
         title: 'Quia dolor sit amet, consectetur, adipisci velit',
         date: '05/02/2023',
         imageURL: 'https://via.placeholder.com/800x700',
-        text: 'Donec sollicitudin dolor ipsum, placerat tempus urna accumsan rutrum. Curabitur ut semper dolor. Donec commodo dignissim erat, vitae lacinia nunc faucibus vel. Nullam posuere quis turpis eu blandit. Cras feugiat malesuada elementum. Nunc posuere, dolor sed luctus tempus, lorem nunc suscipit nulla, ut vehicula ante est vel felis. Proin quis lectus nibh. Proin et pulvinar odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus nec ex non consequat. Cras suscipit aliquet ullamcorper. Nulla facilisi. Maecenas ut neque sit amet eros rhoncus luctus quis sed metus.'
+        text: '<p>Donec sollicitudin dolor ipsum, placerat tempus urna accumsan rutrum. Curabitur ut semper dolor. Donec commodo dignissim erat, vitae lacinia nunc faucibus vel. Nullam posuere fermentum diam quis turpis eu blandit.</p><p>Cras feugiat malesuada elementum. Nunc posuere, dolor sed luctus tempus, lorem nunc suscipit nulla, ut vehicula ante est vel felis. Proin quis lectus nibh. Proin et pulvinar odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus nec ex non consequat.</p><p>Cras suscipit aliquet ullamcorper. Nulla facilisi. Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas ut neque sit amet eros rhoncus luctus quis sed metus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.</p>'
       }
     ]
   }
+
+  cargarNoticias (): string{
+    this.listadoHTML = "";
+    this.listadoHTML= this.arrNoticias.reduce((acc: string, noticia: any): string => {
+      console.log(acc, 'reduce');
+      return acc + `<article class="noticiaWrapper">
+      <div class="imagen"><img class="imageResonsive" src="${noticia.imageURL}" /></div>
+      <div class="noticiaContent">
+        <h2>${noticia.title}</h2>
+        <p class="date">${noticia.date}</p>
+        <p>${noticia.text}</p>
+      </div>
+    </article>`;
+    }, "")
+    return this.listadoHTML;
+  }
+  ;
 }
