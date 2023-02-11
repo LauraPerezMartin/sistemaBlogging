07a5d1bab5d3823c086812793ba932dd02d664f5
Laura Perez <laurap48@hotmail.com>
2023-02-11 12:39:39 +0100

    Recoger datos del formulario, gardarlos en el array y pintar la noticia
    Recoger los datos del formulario, validar los campos vacios, insertar los datos en el array y pintar la noticia.
    Cambios de estilos en style.css y blog.component.css

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -23,23 +23,23 @@ export class BlogComponent {
         id: 1,
         title: 'Neque porro quisquam est qui dolorem ipsum',
         date: '04/02/2023',
-        imageURL: 'https://via.placeholder.com/800',
-        text: '<p> hendrerit est id finibus condimentum. Curabitur malesuada justo at ligula varius ultricies. Duis a nisl ligula. Praesent arcu lorem, sagittis at metus non, posuere fermentum diam. Quisque mollis arcu eu sapien fringilla malesuada. Nulla rhoncus erat at urna semper, id dignissim felis ultricies. Sed nec urna eu arcu fermentum diam euismod aliquam eu sed nisi.</p><p>Donec ultrices sollicitudin arcu, at aliquam tellus ornare eu alesuada elementum. Etiam viverra aliquet sollicitudin. Integer tempus urna ipsum. Aenean turpis nisi, scelerisque vitae nulla non, malesuada tincidunt massa.</p><p>Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas gravida, nisl id accumsan dapibus, metus neque viverra quam, in aliquet ex mauris rhoncus lacus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.</p>'
+        imageURL: 'https://img.freepik.com/vector-premium/ilustracion-literatura-diseno-plano-dibujado-mano_23-2149300321.jpg?w=740',
+        text: '<p> hendrerit est id finibus condimentum. Curabitur malesuada justo at ligula varius ultricies. Duis a nisl ligula. Praesent arcu lorem, sagittis at metus non, posuere fermentum diam. Quisque mollis arcu eu sapien fringilla malesuada. Nulla rhoncus erat at urna semper, id dignissim felis ultricies. Sed nec urna eu arcu fermentum diam euismod aliquam eu sed nisi.</p><p>Donec ultrices sollicitudin arcu, at aliquam tellus ornare eu alesuada elementum. Etiam viverra aliquet sollicitudin. Integer tempus urna ipsum. Aenean turpis nisi, scelerisque vitae nulla non, malesuada tincidunt massa.</p><p>Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas gravida, nisl id accumsan dapibus, metus neque viverra quam, in aliquet ex mauris rhoncus lacus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.</p><p>Donec ultrices sollicitudin arcu, at aliquam tellus ornare eu alesuada elementum. Etiam viverra aliquet sollicitudin. Integer tempus urna ipsum. Aenean turpis nisi, scelerisque.</p>'
       },
       {
         id: 2,
         title: 'Quia dolor sit amet, consectetur, adipisci velit',
         date: '05/02/2023',
-        imageURL: 'https://via.placeholder.com/800x700',
-        text: '<p>Donec sollicitudin dolor ipsum, placerat tempus urna accumsan rutrum. Curabitur ut semper dolor. Donec commodo dignissim erat, vitae lacinia nunc faucibus vel. Nullam posuere fermentum diam quis turpis eu blandit.</p><p>Cras feugiat malesuada elementum. Nunc posuere, dolor sed luctus tempus, lorem nunc suscipit nulla, ut vehicula ante est vel felis. Proin quis lectus nibh. Proin et pulvinar odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus nec ex non consequat.</p><p>Cras suscipit aliquet ullamcorper. Nulla facilisi. Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas ut neque sit amet eros rhoncus luctus quis sed metus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.</p>'
+        imageURL: 'https://img.freepik.com/vector-gratis/ilustracion-criatura-creativa-colorida_516247-6.jpg?w=740&t=st=1676111178~exp=1676111778~hmac=166d69f1c48888c556d22c377e4c05d6c7be60dbb8a457eeb0dafb4462347413',
+        text: '<p>Donec sollicitudin dolor ipsum, placerat tempus urna accumsan rutrum. Curabitur ut semper dolor. Donec commodo dignissim erat, vitae lacinia nunc faucibus vel. Nullam posuere fermentum diam quis turpis eu blandit.</p><p>Cras feugiat malesuada elementum. Nunc posuere, dolor sed luctus tempus, lorem nunc suscipit nulla, ut vehicula ante est vel felis. Proin quis lectus nibh. Proin et pulvinar odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus nec ex non consequat.</p><p>Cras suscipit aliquet ullamcorper. Nulla facilisi. Donec porttitor erat non elit efficitur, eget volutpat augue blandit. Maecenas ut neque sit amet eros rhoncus luctus quis sed metus. Nunc ante felis, feugiat sit amet suscipit sit amet, eleifend non nibh.</p><p>Donec ultrices sollicitudin arcu, at aliquam tellus ornare eu alesuada elementum. Etiam viverra aliquet sollicitudin. Integer tempus urna ipsum. Aenean turpis nisi, scelerisque vitae nulla non, malesuada tincidunt massa.</p>'
       }
     ]
+    this.cargarNoticias (); //añado noticias a listadoHTML para pintarlas la primera vez
   }
 
-  cargarNoticias (): string{
+  cargarNoticias (): void {
     this.listadoHTML = "";
-    this.listadoHTML= this.arrNoticias.reduce((acc: string, noticia: any): string => {
-      console.log(acc, 'reduce');
+    this.listadoHTML= this.arrNoticias.reduce((acc: string, noticia): string => {
       return acc + `<article class="noticiaWrapper">
       <div class="imagen"><img class="imageResonsive" src="${noticia.imageURL}" /></div>
       <div class="noticiaContent">
@@ -49,7 +49,39 @@ export class BlogComponent {
       </div>
     </article>`;
     }, "")
-    return this.listadoHTML;
   }
-  ;
+
+  guardarNoticia(): void {
+    if (this.titulo !== "" && this.fecha !== "" && this.imagenURL !== "" && this.texto ) {
+      const fechaOrdenada: string = this.ordenarFecha(this.fecha); //ordeno la fecha que viene al reves
+
+      let noticia: Noticia= {
+        id: this.contId,
+        title: this.titulo,
+        date: fechaOrdenada,
+        imageURL: this.imagenURL,
+        text: this.texto
+      }
+      
+      this.arrNoticias.push(noticia);
+      this.cargarNoticias ();
+
+      this.contId ++; //una vez añadida la noticia al array inicializo las variables
+      this.titulo = "";
+      this.fecha = "";
+      this.imagenURL = "";
+      this.texto = "";
+
+    } else {
+      alert ('Tienes que rellenar todos los campos para guardar la noticia');
+    }
+ 
+  }
+
+  ordenarFecha(fecha: string): string { //ordeno la fecha
+    const arrFecha = fecha.split('-');
+    arrFecha.reverse();
+    return arrFecha.join('/');
+  }
+  
 }
